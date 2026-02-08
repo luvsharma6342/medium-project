b47a90e41a3a8c406306546fa96ec0da99a1ce63
Luv Sharma <luvsharma105@gmail.com>
2026-02-08 18:10:38 +0530

    Added plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite' // Ensure this is imported
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite"; // Ensure this is imported
 
 export default defineConfig({
   plugins: [
     tailwindcss(), // Ensure this is called here
   ],
-})
+  base: process.env.VITE_BASE_PATH || "/medium-project",
+});
